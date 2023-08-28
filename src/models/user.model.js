627b4d167302cfa474862fc0d52c19cabb0877ac
Diego Prieto <prieto.diego@proton.me>
2023-08-28 17:42:44 +0200

    Added userSchema.index and the nft model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,69 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
+const nftSchema = new Schema({
+    tokenId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    rarity: {
+        type: String,
+        enum: ['Intern', 'Trainee', 'Junior', 'Associate', 'Specialist', 'Manager', 'Senior Manager', 'Director', 'Vice President', 'CEO'],
+        required: true
+    },
+    incomePerHour: {
+        type: Number,
+        required: true
+    }
+});
+
+nftSchema.pre('save', function (next) {
+    switch (this.rarity) {
+        case 'Intern':
+            this.incomePerHour = 500;
+            break;
+        case 'Trainee':
+            this.incomePerHour = 1000;
+            break;
+        case 'Junior':
+            this.incomePerHour = 1500;
+            break;
+        case 'Associate':
+            this.incomePerHour = 2000;
+            break;
+        case 'Specialist':
+            this.incomePerHour = 2500;
+            break;
+        case 'Manager':
+            this.incomePerHour = 3000;
+            break;
+        case 'Senior Manager':
+            this.incomePerHour = 3500;
+            break;
+        case 'Director':
+            this.incomePerHour = 4000;
+            break;
+        case 'Vice President':
+            this.incomePerHour = 4500;
+            break;
+        case 'CEO':
+            this.incomePerHour = 5000;
+            break;
+        default:
+            break;
+    }
+    next();
+});
+
+const NFT = mongoose.model('NFT', nftSchema);
+
+
+
 const userSchema = new Schema({
     username: {
         type: String,
@@ -13,6 +76,14 @@ const userSchema = new Schema({
         type: String, 
         unique: true, 
         required: true 
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/.+@.+\..+/, 'Por favor ingrese una dirección de email válida']  // Validación simple de formato de email.
     },
     password: {
         type: String,
@@ -49,5 +120,9 @@ const userSchema = new Schema({
     }],
 });
 
+userSchema.index({ username: 1, walletAddress: 1, email: 1 });
+
 const User = mongoose.model('User', userSchema);
 export default User;
+
+
