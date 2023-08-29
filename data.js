043294841bd24717828dfad1e3af05124bae3e58
Diego Prieto <prieto.diego@proton.me>
2023-08-29 07:41:19 +0200

    Added info for each type of company that can be created. Build cost, build time, upgrade cost, income per hour.

---
+++ data.js
@@ -0,0 +1,79 @@
+const companyCostAndTime = {
+    "Agriculture": {
+      buildCost: 5000,
+      buildTime: '1h',
+      upgradeCosts: [2000, 3000, 4000, 5000],
+      upgradeTimes: ['30m', '45m', '1h', '1h 30m'],
+      incomePerHour: [300, 600, 900, 1200, 1500]
+    },
+    "Livestock": {
+      buildCost: 6000,
+      buildTime: '1h 15m',
+      upgradeCosts: [2500, 3500, 4500, 5500],
+      upgradeTimes: ['40m', '55m', '1h 10m', '1h 25m'],
+      incomePerHour: [350, 700, 1050, 1400, 1750]
+    },
+    "Mining": {
+      buildCost: 10000,
+      buildTime: '2h',
+      upgradeCosts: [4000, 6000, 8000, 10000],
+      upgradeTimes: ['1h', '1h 30m', '2h', '2h 30m'],
+      incomePerHour: [500, 1000, 1500, 2000, 2500]
+    },
+    "Hotels": {
+      buildCost: 15000,
+      buildTime: '3h',
+      upgradeCosts: [6000, 9000, 12000, 15000],
+      upgradeTimes: ['1h 30m', '2h 15m', '3h', '3h 45m'],
+      incomePerHour: [750, 1500, 2250, 3000, 3750]
+    },
+    "Restaurants": {
+      buildCost: 8000,
+      buildTime: '1h 45m',
+      upgradeCosts: [3200, 4800, 6400, 8000],
+      upgradeTimes: ['50m', '1h 15m', '1h 40m', '2h 5m'],
+      incomePerHour: [400, 800, 1200, 1600, 2000]
+    },
+    "Bars": {
+      buildCost: 7000,
+      buildTime: '1h 30m',
+      upgradeCosts: [2800, 4200, 5600, 7000],
+      upgradeTimes: ['45m', '1h 7m', '1h 30m', '1h 52m'],
+      incomePerHour: [350, 700, 1050, 1400, 1750]
+    },
+    "Nightclubs": {
+      buildCost: 20000,
+      buildTime: '4h',
+      upgradeCosts: [8000, 12000, 16000, 20000],
+      upgradeTimes: ['2h', '3h', '4h', '5h'],
+      incomePerHour: [1000, 2000, 3000, 4000, 5000]
+    },
+    "ClothingBrands": {
+      buildCost: 12000,
+      buildTime: '2h 30m',
+      upgradeCosts: [4800, 7200, 9600, 12000],
+      upgradeTimes: ['1h 15m', '1h 52m', '2h 30m', '3h 7m'],
+      incomePerHour: [600, 1200, 1800, 2400, 3000]
+    },
+    "Automotive": {
+      buildCost: 25000,
+      buildTime: '5h',
+      upgradeCosts: [10000, 15000, 20000, 25000],
+      upgradeTimes: ['2h 30m', '3h 45m', '5h', '6h 15m'],
+      incomePerHour: [1250, 2500, 3750, 5000, 6250]
+    },
+    "Tech": {
+      buildCost: 30000,
+      buildTime: '6h',
+      upgradeCosts: [12000, 18000, 24000, 30000],
+      upgradeTimes: ['3h', '4h 30m', '6h', '7h 30m'],
+      incomePerHour: [1500, 3000, 4500, 6000, 7500]
+    },
+    "Finance": {
+      buildCost: 40000,
+      buildTime: '8h',
+      upgradeCosts: [16000, 24000, 32000, 40000],
+      upgradeTimes: ['4h', '6h', '8h', '10h'],
+      incomePerHour: [2000, 4000, 6000, 8000],
+}
+}  
