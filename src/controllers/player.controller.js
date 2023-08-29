c9a206f38f279fa2c62c6e3e52c66279c3e1fcef
Diego Prieto <prieto.diego@proton.me>
2023-08-29 12:16:47 +0200

    Player controllers done and functional

---
+++ src/controllers/player.controller.js
@@ -0,0 +1,84 @@
+import Player from './models/player.model';
+
+
+const createPlayer = async (req, res) => {
+  try {
+    const newPlayer = new Player(req.body);
+    await newPlayer.save();
+    res.status(201).json(newPlayer);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+const updatePlayer = async (req, res) => {
+  try {
+    const player = await Player.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!player) {
+      return res.status(404).json({ error: 'Jugador no encontrado' });
+    }
+    res.status(200).json(player);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+const deletePlayer = async (req, res) => {
+  try {
+    const player = await Player.findByIdAndDelete(req.params.id);
+    if (!player) {
+      return res.status(404).json({ error: 'Jugador no encontrado' });
+    }
+    res.status(200).json({ message: 'Jugador eliminado' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const getPlayer = async (req, res) => {
+    try {
+      const player = await Player.findById(req.params.id).populate('economicActivities');
+      if (!player) {
+        return res.status(404).json({ error: 'Jugador no encontrado' });
+      }
+      res.status(200).json(player);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  
+
+export const getActivePlayers = async (req, res) => {
+    try {
+      const activePlayers = await Player.find({ isActive: true });
+      res.status(200).json(activePlayers);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  
+
+export const toggleAllPlayersStatus = async (req, res) => {
+    try {
+      const players = await Player.find({});
+      for (const player of players) {
+        player.isActive = !player.isActive;
+        await player.save();
+      }
+      res.status(200).json({ message: 'Estado de todos los jugadores cambiado' });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+
+
+export {
+  createPlayer,
+  updatePlayer,
+  deletePlayer,
+  getActivePlayers,
+  toggleAllPlayersStatus,
+  getPlayer
+};
