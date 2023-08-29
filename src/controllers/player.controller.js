f06b511f74606caa051cb2c92190f2009c44f84b
Diego Prieto <prieto.diego@proton.me>
2023-08-29 15:00:30 +0200

    All the player controllers improved and functioning

--- src/controllers/player.controller.js
+++ src/controllers/player.controller.js
@@ -3,42 +3,95 @@ import EconomicAct from '../models/economicAct.model.js';
 
 
 export const createPlayer = async (req, res) => {
-  try {
-    const newPlayer = new Player(req.body);
-    await newPlayer.save();
-    res.status(201).json(newPlayer);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
 
+    const { username, income, inGameTokens, isActive } = req.body;
+  
+    if (!username || username.length < 3) {
+      return res.status(400).json({ error: 'El nombre de usuario es requerido y debe tener al menos 3 caracteres.' });
+    }
+
+  
+    try {
+      const existingPlayer = await Player.findOne({ username });
+      if (existingPlayer) {
+        return res.status(400).json({ error: 'El nombre de usuario ya existe.' });
+      }
+  
+      const newPlayer = new Player({
+        username,
+        income: income || 200,
+        inGameTokens: inGameTokens || 0,
+        isActive: isActive !== undefined ? isActive : true
+      });
+  
+      await newPlayer.save();
+      res.status(201).json(newPlayer);
+      
+    } catch (error) {
+      if (error.name === 'ValidationError') {
+        res.status(400).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: 'Error del servidor al crear el jugador.' });
+      }
+    }
+  };
+  
 
 export const updatePlayer = async (req, res) => {
+  const { username, income, inGameTokens, isActive } = req.body;
+
+  if (username && username.length < 3) {
+    return res.status(400).json({ error: 'El nombre de usuario debe tener al menos 3 caracteres.' });
+  }
+
+
   try {
-    const player = await Player.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!player) {
+    const existingPlayer = await Player.findById(req.params.id);
+    if (!existingPlayer) {
       return res.status(404).json({ error: 'Jugador no encontrado' });
     }
-    res.status(200).json(player);
+
+    if (username) existingPlayer.username = username;
+    if (income) existingPlayer.income = income;
+    if (inGameTokens !== undefined) existingPlayer.inGameTokens = inGameTokens;
+    if (isActive !== undefined) existingPlayer.isActive = isActive;
+
+    await existingPlayer.save();
+
+    res.status(200).json(existingPlayer);
+    
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    if (error.name === 'ValidationError') {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: 'Error del servidor al actualizar el jugador.' });
+    }
   }
 };
 
 
 export const deletePlayer = async (req, res) => {
-  try {
-    const player = await Player.findByIdAndDelete(req.params.id);
-    if (!player) {
-      return res.status(404).json({ error: 'Jugador no encontrado' });
-    }
-    res.status(200).json({ message: 'Jugador eliminado' });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+    try {
+      const player = await Player.findById(req.params.id);
+      if (!player) {
+        return res.status(404).json({ error: 'Jugador no encontrado' });
+      }
 
-export const getPlayer = async (req, res) => {
+      await player.remove();
+  
+      res.status(200).json({ message: 'Jugador eliminado exitosamente' });
+    } catch (error) {
+      if (error.name === 'CastError') {
+        res.status(400).json({ error: 'ID de jugador inválido' });
+      } else {
+        res.status(500).json({ error: 'Error del servidor al eliminar el jugador' });
+      }
+    }
+  };
+  
+
+
+  export const getPlayer = async (req, res) => {
     try {
       const player = await Player.findById(req.params.id).populate('economicActivities');
       if (!player) {
@@ -46,31 +99,33 @@ export const getPlayer = async (req, res) => {
       }
       res.status(200).json(player);
     } catch (error) {
-      res.status(400).json({ error: error.message });
+      if (error.name === 'CastError') {
+        res.status(400).json({ error: 'ID de jugador inválido' });
+      } else {
+        res.status(500).json({ error: 'Error del servidor al obtener el jugador' });
+      }
     }
   };
+  
   
 
   export const getActivePlayers = async (req, res) => {
     try {
-      const activePlayers = await Player.find({ isActive: true });
+      const limit = parseInt(req.query.limit) || 50; 
+      const activePlayers = await Player.find({ isActive: true }).limit(limit);
       res.status(200).json(activePlayers);
     } catch (error) {
-      res.status(400).json({ error: error.message });
+      res.status(500).json({ error: 'Error del servidor al obtener los jugadores activos' });
     }
   };
   
 
   export const toggleAllPlayersStatus = async (req, res) => {
     try {
-      const players = await Player.find({});
-      for (const player of players) {
-        player.isActive = !player.isActive;
-        await player.save();
-      }
+      await Player.updateMany({}, [{ $set: { isActive: { $not: "$isActive" } } }]);
       res.status(200).json({ message: 'Estado de todos los jugadores cambiado' });
     } catch (error) {
-      res.status(400).json({ error: error.message });
+      res.status(500).json({ error: 'Error del servidor al cambiar el estado de los jugadores' });
     }
   };
-
+  
