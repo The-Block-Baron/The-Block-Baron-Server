8b828c1a03f6999eca86fcd2dd3993f403d927d0
Diego Prieto <prieto.diego@proton.me>
2023-08-29 07:11:48 +0200

    State model created

---
+++ src/models/state.model.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const stateSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  president: {
+    npcName: {
+      type: String,
+      required: true
+    },
+    personality: {
+      type: String,
+      enum: ['Aggressive', 'Passive', 'Neutral', 'Charismatic', 'Manipulative'],
+      required: true
+    }
+  },
+  taxRate: {
+    type: Number,
+    required: true,
+    default: 0.1
+  },
+  companies: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Company'
+  }],
+  playerCompanies: [{
+    player: {
+      type: Schema.Types.ObjectId,
+      ref: 'Player'
+    },
+    companies: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Company'
+    }]
+  }],
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const State = mongoose.model('State', stateSchema);
+
+export default State;
