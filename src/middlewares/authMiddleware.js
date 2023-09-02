446fca9eff46a985fa51ae1d5cc24ec7dd310550
Diego Prieto <prieto.diego@proton.me>
2023-09-02 09:03:08 +0200

    authMiddleware created

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Authorization header missing' });
+    }
+
+    const token = authHeader.split(' ')[1]; 
+
+    if (!token) {
+        return res.status(401).json({ message: 'Token missing' });
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decodedToken;
+
+        next();
+    } catch (error) {
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+        return res.status(500).json({ message: 'An internal server error occurred' });
+    }
+};
+
+export default authMiddleware;
